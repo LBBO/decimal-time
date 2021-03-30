dee407d8c206626766c5021adbbb6f95b629a672
Michael Kuckuk <mdk@kuckuk.com>
2021-03-30 18:51:33 +0200

    Create own module for clock

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NavigationComponent } from './navigation/navigation.component'
 import { MatToolbarModule } from '@angular/material/toolbar'
 import { LandingpageRoutingModule } from './landingpage/landingpage-routing.module'
-import { ClockComponent } from './clock/clock.component'
+import { ClockModule } from './clock/clock.module'
 
 @NgModule({
-  declarations: [AppComponent, NavigationComponent, ClockComponent],
+  declarations: [AppComponent, NavigationComponent],
   imports: [
     BrowserModule,
     LandingpageRoutingModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    ClockModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
-  exports: [ClockComponent],
+  exports: [],
 })
 export class AppModule {}
