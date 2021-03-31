f31992aa433be1e37612b99f141e00f6b96276a3
Michael Kuckuk <mdk@kuckuk.com>
2021-03-31 22:55:03 +0200

    Add french history

--- src/app/history/history.module.ts
+++ src/app/history/history.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { HistoryRoutingModule } from './history-routing.module';
+import { HistoryRoutingModule } from './history-routing.module'
 import { HistoryComponent } from './history.component'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
 
 @NgModule({
   declarations: [HistoryComponent],
-  imports: [CommonModule, HistoryRoutingModule],
+  imports: [CommonModule, HistoryRoutingModule, MatCardModule, MatButtonModule],
 })
 export class HistoryModule {}
