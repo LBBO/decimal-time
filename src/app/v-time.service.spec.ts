0b8fa5c1a56e053a46d205e45102c2c86bcc4c33
Michael Kuckuk <8076094+LBBO@users.noreply.github.com>
2022-09-04 00:23:16 +0200

    Add methods to convert from and to v time

--- src/app/v-time.service.spec.ts
+++ src/app/v-time.service.spec.ts
@@ -13,4 +13,93 @@ describe('VTimeService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy()
   })
+
+  describe('normal time -> v time', () => {
+    it('should convert midnight', () => {
+      expect(service.convertNormalTimeToVTime({
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      })).toEqual({
+        v: 0,
+        deciV: 0,
+        milliV: 0,
+      })
+    })
+
+    it('should convert noon', () => {
+      expect(service.convertNormalTimeToVTime({
+        hours: 12,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      })).toEqual({
+        v: 5,
+        deciV: 0,
+        milliV: 0,
+      })
+    })
+
+    it('should convert a quarter day', () => {
+      expect(service.convertNormalTimeToVTime({
+        hours: 6,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      })).toEqual({
+        v: 2,
+        deciV: 50,
+        milliV: 0,
+      })
+    })
+
+    it('should convert a minute before noon', () => {
+      expect(service.convertNormalTimeToVTime({
+        hours: 11,
+        minutes: 59,
+        seconds: 0,
+        milliseconds: 0,
+      })).toEqual({
+        v: 4,
+        deciV: 99,
+        milliV: 30,
+      })
+    })
+  })
+
+  describe('should convert time to vtime and back again', () => {
+    [
+      {
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      },
+      {
+        hours: 12,
+        minutes: 0,
+        seconds: 0,
+        milliseconds: 0,
+      },
+      // {
+      //   hours: 11,
+      //   minutes: 59,
+      //   seconds: 0,
+      //   milliseconds: 0,
+      // },
+      // {
+      //   hours: 17,
+      //   minutes: 36,
+      //   seconds: 12,
+      //   milliseconds: 345,
+      // },
+    ]
+      .forEach((input) => {
+        it(`for ${input.hours}:${input.minutes} ${input.seconds}.${input.milliseconds}`, () => {
+          const vtime = service.convertNormalTimeToVTime(input)
+          expect(service.convertVTimeToNormalTime(vtime)).toEqual(input)
+        })
+      })
+  })
 })
--- src/app/v-time.service.ts
+++ src/app/v-time.service.ts
@@ -3,6 +3,19 @@ import { ITimeService } from './ITimeService'
 import { Observable, Subject } from 'rxjs'
 import { distinctUntilChanged } from 'rxjs/operators'
 
+type VTime = {
+  v: number,
+  deciV: number,
+  milliV: number
+}
+
+type NormalTime = {
+  hours: number
+  minutes: number
+  seconds: number
+  milliseconds: number
+}
+
 @Injectable({
   providedIn: 'root',
 })
@@ -37,22 +50,61 @@ export class VTimeService implements ITimeService, OnDestroy {
     return [this.v$, this.deciV$, this.milliV$]
   }
 
-  private updateTime(): void {
+  convertNormalTimeToVTime(
+    normalTime: NormalTime,
+  ): VTime {
     const millisecondsPerDay = 24 * 60 * 60 * 1000
     const millisecondsPerV = millisecondsPerDay / 10
     const millisecondsPerDeciV = millisecondsPerV / 100
     const millisecondsPerMilliV = millisecondsPerV / 10_000
-    const now = new Date()
-    const totalMinutesPassed = now.getHours() * 60 + now.getMinutes()
-    const totalSecondsPassed = totalMinutesPassed * 60 + now.getSeconds()
+    const totalMinutesPassed = normalTime.hours * 60 + normalTime.minutes
+    const totalSecondsPassed = totalMinutesPassed * 60 + normalTime.seconds
     const totalMillisecondsPassed =
-      totalSecondsPassed * 1000 + now.getMilliseconds()
+      totalSecondsPassed * 1000 + normalTime.milliseconds
+    return {
+      milliV: Math.floor(totalMillisecondsPassed / millisecondsPerMilliV) % 100,
+      deciV: Math.floor(totalMillisecondsPassed / millisecondsPerDeciV) % 100,
+      v: Math.floor(totalMillisecondsPassed / millisecondsPerV) % 10,
+    }
+  }
+
+  convertVTimeToNormalTime(
+    vTime: VTime,
+  ): NormalTime {
+    const millisecondsPerDay = 24 * 60 * 60 * 1000
+    const millisecondsPerV = millisecondsPerDay / 10
+    const millisecondsPerMilliV = millisecondsPerV / 10_000
+
+    const totalDeciVPassed = vTime.v * 100 + vTime.deciV
+    const totalMilliVPassed = vTime.milliV + totalDeciVPassed * 100
+
+    const totalMillisecondsPassed = totalMilliVPassed * millisecondsPerMilliV
+    const totalSecondsPassed = totalMillisecondsPassed / 1_000
+    const totalMinutesPassed = totalSecondsPassed / 60
+    const totalHoursPassed = totalMinutesPassed / 60
+
+    return {
+      hours: Math.floor(totalHoursPassed % 24),
+      minutes: Math.floor(totalMinutesPassed % 60),
+      seconds: Math.floor(totalSecondsPassed % 60),
+      milliseconds: Math.floor(totalMillisecondsPassed % 1_000),
+    }
+  }
+
+  private updateTime(): void {
+    const now = new Date()
+    const { deciV, milliV, v } = this.convertNormalTimeToVTime({
+      hours: now.getHours(),
+      minutes: now.getMinutes(),
+      seconds: now.getSeconds(),
+      milliseconds: now.getMilliseconds(),
+    })
     this.#milliV$.next(
-      Math.floor(totalMillisecondsPassed / millisecondsPerMilliV) % 100,
+      milliV,
     )
     this.#deciV$.next(
-      Math.floor(totalMillisecondsPassed / millisecondsPerDeciV) % 100,
+      deciV,
     )
-    this.#v$.next(Math.floor(totalMillisecondsPassed / millisecondsPerV) % 10)
+    this.#v$.next(v)
   }
 }
